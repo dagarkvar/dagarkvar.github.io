35106bcd0baa0146d906f3d86a8fc4e9b3d90443
DagarKvar <karl.lexander@gmail.com>
2017-12-21 10:12:32 +0100

    0.0.5

--- js/dates.js
+++ js/dates.js
@@ -9,33 +9,10 @@ var date = {
 var lovdist = date.springbegin.getTime() - new Date().getTime()
 var lovdays = Math.floor(lovdist / (1000 * 60 * 60 * 24));
 
-
-
-//var now = date.jullov;
-//var start = new Date(now.getFullYear(), 0, 0);
-//var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
-//var oneDay = 1000 * 60 * 60 * 24;
-//var day2 = Math.floor(diff / oneDay);
-
-//var now = date.springbegin;
-//var start = new Date(now.getFullYear(), 0, 0);
-//var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
-//var oneDay = 1000 * 60 * 60 * 24;
-//var day = Math.floor(diff / oneDay);
-
 var lovdist = date.springbegin.getTime() - date.jullov.getTime()
 var terminlangd = Math.floor(lovdist / (1000 * 60 * 60 * 24));
 console.log(terminlangd)
 
-//var terminlangd = day - day2
-
-
-//var now = new Date();
-//var start = new Date(now.getFullYear(), 0, 0);
-//var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
-//var oneDay = 1000 * 60 * 60 * 24;
-//var day3 = Math.floor(diff / oneDay);
-//console.log(day3)
 
 var lovdist = date.springbegin.getTime() - new Date().getTime()
 var day4 = Math.floor(lovdist / (1000 * 60 * 60 * 24));
